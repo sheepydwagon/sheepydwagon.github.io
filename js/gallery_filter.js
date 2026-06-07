079ccd8cc771c9385b4a92aa003be94f0c2fca12
sheepydwagon <sheepydragonmail@gmail.com>
2026-06-06 23:39:56 -0500

    Updated several pages and added some images

---
+++ js/gallery_filter.js
@@ -0,0 +1,47 @@
+// altered from: https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("gallery");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "visible");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "visible");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+//   element.setAttribute("data-lightbox","current");
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+//   element.setAttribute("data-lightbox",null);
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("galleryButtons");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
